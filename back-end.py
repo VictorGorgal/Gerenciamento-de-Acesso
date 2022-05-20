db15080eb44abd332f83c4731b0200214075f588
Victor Gorgal <victorgorgal@gmail.com>
2022-05-20 11:39:26 -0300

    adicionou banco de dados

---
+++ back-end.py
@@ -0,0 +1,65 @@
+import sqlite3
+from os.path import exists
+from datetime import datetime
+
+
+class gerenciamento:
+    def __init__(self, file):
+        self.database = file
+
+        self.initDB()
+
+    def save_usuario(self, data):
+        _, nome, senha, admin = data.split(';')
+
+        self.connection = sqlite3.connect(self.database)
+        cursor = self.connection.cursor()
+
+        cursor.execute('insert into usuarios (nome, senha, admin) values (?, ?, ?)', [nome, senha, admin])
+
+        self.connection.commit()
+        self.connection.close()
+
+    def save_evento(self, data):
+        _, evento = data.split(';')
+
+        now = datetime.now()
+        dia = f'{now.day}/{now.month}/{now.year}'
+        horario = f'{now.hour}/{now.minute}/{now.second}'
+
+        self.connection = sqlite3.connect(self.database)
+        cursor = self.connection.cursor()
+
+        cursor.execute('insert into eventos (data, horario, evento) values (?, ?, ?)', [dia, horario, evento])
+
+        self.connection.commit()
+        self.connection.close()
+
+    def initDB(self):  # cria o database caso ainda nao exista
+        if exists(self.database):
+            return
+
+        print('Criando database...')
+        self.connection = sqlite3.connect(self.database)
+        cursor = self.connection.cursor()
+
+        cursor.execute("""create table usuarios (
+                        nome text, 
+                        senha text, 
+                        admin blob)""")
+
+        cursor.execute("""create table eventos (
+                        data text,
+                        horario text,
+                        evento text)""")
+
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    data = 'usuario;victor arruda;Senha123!@;1'  # (tipo,nome,senha,admin)
+    data2 = 'evento;porta1 aberta'  # (tipo, evento)
+
+    g = gerenciamento('./database.db')
+    g.save_usuario(data)
+    g.save_evento(data2)
